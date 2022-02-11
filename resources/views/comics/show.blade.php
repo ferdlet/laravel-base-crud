ce85f57ac528adb694c6ff9b2a27f69df743624c
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-02-11 15:48:11 +0100

    push

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,9 +6,11 @@
 
 @section('pageContent')
 
-    <h2>{{$comic->title}}</h2>
-    <p>{{$comic->description}}</p>
-    <img src="{{$comic->thumb}}" alt="">
-    <h3>€ {{$comic->price}}</h3>
-    <a href="{{route('comics.index')}}">Torna ai comics</a>
+    <div class="container">
+        <h2>{{$comic->title}}</h2>
+        <p>{{$comic->description}}</p>
+        <img src="{{$comic->thumb}}" alt="">
+        <h3>€ {{$comic->price}}</h3>
+        <a href="{{route('comics.index')}}">Torna ai comics</a>
+    </div>
 @endsection
